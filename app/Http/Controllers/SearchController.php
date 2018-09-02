02e9bccc38cb4edb85d735ad8da34d45af6255f2
Brian Bruno <brian.bruno@outlook.com>
2018-09-01 21:31:25 -0300

    first commit

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: brian
+ * Date: 24/06/2018
+ * Time: 10:43
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+
+class SearchController extends Controller {
+
+    public function index($request) {
+        $url = $request->input('inputURL');
+        $view = view('dashboard');
+
+        if (isset($url)) {
+            $resultado = $this->buscarURL($url);
+            if (sizeof($resultado) > 0)
+                $view = view('busca', ['resultados' => $resultado->jsonSerialize()]);
+        }
+        return $view;
+    }
+
+    public function buscarURL($url) {
+        $registro = DB::table('url')
+            ->select('ID', 'URL', 'ID_ORIGEM')
+            ->whereRaw('URL LIKE "%'.$url.'%"')
+            ->get();
+
+        if (sizeof($registro) > 0)
+            return $registro;
+        else
+            return null;
+    }
+
+    public function buscarOrigem($id) {
+        $array = array();
+        $id_origem = $id;
+
+        while ($id_origem != null) {
+            $origem = DB::table('url')
+                ->select('ID', 'URL', 'ID_ORIGEM')
+                ->where('ID', $id_origem)
+                ->get();
+            $id_origem = $origem[0]->ID_ORIGEM;
+            $array[] = $origem[0]->URL;
+        }
+
+        return $array;
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::post('/buscar', function (Illuminate\Http\Request $request) {
+    $homeController = app()->make('\App\Http\Controllers\SearchController');
+    return $homeController->index($request);
+})->name('buscar');
+
+Route::get('/find/{id}', function ($id) {
+    $homeController = app()->make('\App\Http\Controllers\SearchController');
+    $resultado = $homeController->buscarOrigem($id);
+    return view('resultado', ['resultados' => array_reverse($resultado)]);
+})->name('find');
